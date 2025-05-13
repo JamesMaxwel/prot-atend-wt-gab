5a0516095dd8480f68413e32c15fe2a346b45caf
James Maxwel <kpzdev@proton.me>
2025-05-12 22:59:43 -0300

    Configuração para deploy no GitHub Pages

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  output: 'export',
+  basePath: '/prot-atend-wt-gab',
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig; 
